7e6c07262684800aafbe8d3a43c891501b77f1a2
kayvansol <kayvan.sol2@gmail.com>
2024-06-02 00:07:29 +0330

    second edition

---
+++ a.py
@@ -0,0 +1,24 @@
+import socket
+import sys
+
+HOST = '192.168.1.4'
+PORT = 50000
+
+try:
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error as msg:
+  sys.stderr.write("[ERROR] %s\n" % msg[1])
+  sys.exit(1)
+
+try:
+  sock.connect((HOST, PORT))
+except socket.error as msg:
+  sys.stderr.write("[ERROR] %s\n" % msg[1])
+  sys.exit(2)
+
+msg = '{"id":"1","name":"kayvan soleimani","email":"kayvan.sol2@gmail.com}'
+
+sock.send(msg.encode('utf-8') )
+
+sock.close()
+sys.exit(0)
